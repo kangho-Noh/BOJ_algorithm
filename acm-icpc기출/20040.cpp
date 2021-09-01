20bc2aa054ec35eb548e490a7229df6f020c2d00
Kangho-Noh <rkdgh98@khu.ac.kr>
2021-09-01 22:55:25 +0900

    Solve SWmembership test2-1

---
+++ acm-icpc기출/20040.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int parent[500001];
+
+int find_parent(int n){
+    if(parent[n]==n) return n;
+    return parent[n] = find_parent(parent[n]);
+}
+
+void union_parent(int n,int m){
+    int pn = find_parent(n);
+    int pm = find_parent(m);
+    if(pn>pm) parent[pm] = pn;
+    else parent[pn] = pm;
+}
+
+int main(){
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    int n,m; cin>>n>>m;
+    int ans = 0;
+    for(int i=0;i<n;i++){
+        parent[i] = i;
+    }
+    for(int i=1;i<=m;i++){
+        int a,b; cin>>a>>b;
+        if(find_parent(a) == find_parent(b))
+        {
+            ans = i;
+            break;
+        }else{
+            union_parent(a,b);
+        }
+    }
+    cout<<ans<<'\n';
+}
